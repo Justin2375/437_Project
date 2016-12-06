c4319254394e6fd9eae13746c1de5d59ead4ca95
Justin2375 <justin.jarrard@siu.edu>
2016-12-05 20:38:31 -0600

    Delete credentials.py

--- credentials.py
+++
@@ -1,6 +0,0 @@
-secret = {
-  'cons_key':'b8cgnMBZ6rSVqZBKAZWe6sjv1',
-  'sec_cons_key' : 's0hvxnxvRBo5KrixJzVTI586ZfdfnWGj3A9TBU4LasJOKciyQ4',
-  'accs_token' : '862000495-NwL4SCvQNn1qdjFVXc8KonixmT1rOJgLF9JKJiKz',
-  'sec_accs_token' : 'Rao8siVXQp9pOIJoxmBvn2QZgZriFhRAh4HnfvFK6QL0g'
-  }
